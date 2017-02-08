cbec46bae498dcdb4d19e818dfc26d30d674348d
heriet <admin@heriet.info>
2017-02-08 19:17:12 +0900

    fix secret arguments no_log=True should be set.

--- library/niftycloud.py
+++ library/niftycloud.py
@@ -313,7 +313,7 @@ def main():
 	module = AnsibleModule(
 		argument_spec = dict(
 			access_key          = dict(required=True,  type='str'),
-			secret_access_key   = dict(required=True,  type='str'),
+			secret_access_key   = dict(required=True,  type='str', no_log=True),
 			endpoint            = dict(required=True,  type='str'),
 			instance_id         = dict(required=True,  type='str'),
 			state               = dict(required=True,  type='str'),
