3c16805c75e377c462ed698f5d74bd97b631eca2
ykokw <0k4w4.yuk1@gmail.com>
2017-04-04 08:54:15 +0000

    Deleted unnecessary functions and test code from volume module

--- library/niftycloud_volume.py
+++ library/niftycloud_volume.py
@@ -54,57 +54,7 @@ EXAMPLES = '''
 - action: niftycloud_lb access_key="YOUR_ACCESS_KEY" secret_access_key="YOUR_SECRET_ACCESS_KEY" endpoint="west-1.cp.cloud.nifty.com" size="100" volume_id="testdisk001" disk_type="3" instance_id="test001" accounting_type="2" state="present"
 '''
 
-def calculate_signature(secret_access_key, method, endpoint, path, params):
-	payload = ""
-	for v in sorted(params.items()):
-		payload += '&{0}={1}'.format(v[0], urllib.quote(str(v[1]), ''))
-	payload = payload[1:]
-
-	string_to_sign = [method, endpoint, path, payload]
-	digest = hmac.new(secret_access_key, '\n'.join(string_to_sign), hashlib.sha256).digest()
-
-	return base64.b64encode(digest)
-
-def request_to_api(module, method, action, params):
-	params['Action']           = action
-	params['AccessKeyId']      = module.params['access_key']
-	params['SignatureMethod']  = 'HmacSHA256'
-	params['SignatureVersion'] = '2'
-
-	path     = '/api/'
-	endpoint = module.params['endpoint']
-
-	params['Signature'] = calculate_signature(module.params['secret_access_key'], method, endpoint, path, params)
-
-	r = None
-	if method == 'GET':
-		url = 'https://{0}{1}?{2}'.format(endpoint, path, urllib.urlencode(params))
-		r = requests.get(url)
-	elif method == 'POST':
-		url = 'https://{0}{1}'.format(endpoint, path)
-		r = requests.post(url, urllib.urlencode(params))
-	else:
-		module.fail_json(status=-1, msg='changes failed (un-supported http method)')
-
-
-	if r is not None:
-		body = r.text.encode('utf-8')
-		xml = etree.fromstring(body)
-		info = dict(
-			status   = r.status_code,
-			xml_body = xml,
-			xml_namespace = dict(nc = xml.tag[1:].split('}')[0])
-		)
-		return info
-	else:
-		module.fail_json(status=-1, msg='changes failed (http request failed)')
-
-def get_api_error(xml_body):
-	info = dict(
-		code    = xml_body.find('.//Errors/Error/Code').text,
-		message = xml_body.find('.//Errors/Error/Message').text
-	)
-	return info
+from niftycloud_client import request_to_api, get_api_error
 
 def get_volume_state(module):
 	params = dict()
@@ -233,7 +183,6 @@ def main():
 
 from ansible.module_utils.basic import *
 import urllib, hmac, hashlib, base64, time, requests
-import xml.etree.ElementTree as etree
 
 if __name__ == '__main__':
 	main()
